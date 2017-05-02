64ab70d2400afb2f8e7f5060765f586b3986565d
lah09 <lahor.skolan@gmail.com>
2017-05-02 13:12:08 +0200

    change - Program

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -3,13 +3,71 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
-namespace Calculator
+using System.Globalization;
+namespace CalcExample
 {
     class Program
     {
         static void Main(string[] args)
         {
+            while (true)
+            {
+                Console.WriteLine("Enter the first number: ");
+                double num1 = GetNumber();
+                Console.WriteLine("Enter the second number: ");
+                double num2 = GetNumber();
+                Console.WriteLine("Enter the operator: ");
+                string strOp = Console.ReadLine();
+                switch (strOp)
+                {
+                    case "+":
+                        Console.WriteLine("the sum of the two numbers is: " + (num1 + num2));
+                        break;
+                    case "-":
+                        Console.WriteLine("the difference of the two numbers is: " + (num1 - num2));
+                        break;
+                    case "*":
+                        Console.WriteLine("the sum of the two numbers is: " + (num1 * num2));
+                        break;
+                    case "/":
+                        if (num2 != 0)
+                        {
+                            Console.WriteLine("the sum of the two numbers is: " + (num1 / num2));
+                        }
+                        else
+                        {
+                            Console.WriteLine("the second value is 0, division by 0 is not allowed.");
+                        }
+                        break;
+                    default:
+                        Console.WriteLine("Invalid Operator. Try again!");
+                        break;
+                }
+                Console.WriteLine("Do you want to continue? (y/n)");
+                if (Console.ReadLine() == "y")
+                {
+                    continue;
+                }
+                else
+                {
+                    break;
+                }
+            }
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey();
+        }
+        static double GetNumber()
+        {
+            double num = 0;
+            while (true)
+            {
+                string numStr = Console.ReadLine();
+                if (double.TryParse(numStr, out num))
+                {
+                    break;
+                }
+            }
+            return num;
         }
     }
 }
